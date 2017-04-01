7613acf8992e7f3adb9767cf13ee8225dc778bc9
DBLJ <jean-alexandre.pecontal@edu.ece.fr>
2017-04-01 23:34:11 +0200

    added files for previous commit ( bug )

---
+++ AndroidProject/app/src/main/java/com/ece/aurelien/androidproject/Match/MatchView.java
@@ -0,0 +1,21 @@
+package com.ece.aurelien.androidproject.Match;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ece.aurelien.androidproject.R;
+
+/**
+ * Created by win8 on 01/04/2017.
+ */
+
+public class MatchView extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.match_informationcorr);
+
+
+    }
+}
