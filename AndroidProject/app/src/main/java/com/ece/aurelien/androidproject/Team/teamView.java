71d5aef16808f5fde3ddfedbacbedff9af387481
DBLJ <jean-alexandre.pecontal@edu.ece.fr>
2017-04-07 18:41:53 +0200

    added files for team

---
+++ AndroidProject/app/src/main/java/com/ece/aurelien/androidproject/Team/teamView.java
@@ -0,0 +1,19 @@
+package com.ece.aurelien.androidproject.Team;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ece.aurelien.androidproject.R;
+
+/**
+ * Created by win8 on 07/04/2017.
+ */
+
+public class teamView extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.teams_composition);
+
+
+    }
+}
