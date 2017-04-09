3d8e2bcab71501d257f0603121ff5a6719778fb3
Sternberger <aurelien.sternberger@gmail.com>
2017-04-09 17:51:28 +0200

    picture fonctionnel ??

--- AndroidProject/app/src/main/java/com/ece/aurelien/androidproject/Camera/CameraActivity.java
+++ AndroidProject/app/src/main/java/com/ece/aurelien/androidproject/Camera/CameraActivity.java
@@ -2,14 +2,24 @@ package com.ece.aurelien.androidproject.Camera;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.ece.aurelien.androidproject.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 /**
@@ -19,6 +29,10 @@ import com.ece.aurelien.androidproject.R;
 public class CameraActivity extends AppCompatActivity{
     ImageView result;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private String mFileLocation = "";
+    File photoFile = null;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -37,9 +51,22 @@ public class CameraActivity extends AppCompatActivity{
 
     public void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //File file = getFile();
+
+        //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(getApplicationContext(), "com.Camera.android.fileprovider",file));
+
+        File photoFile = null;
+        try{
+            photoFile = createImageFile();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
+
     }
 
     @Override
@@ -47,8 +74,38 @@ public class CameraActivity extends AppCompatActivity{
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
+            try {
+                FileOutputStream out = new FileOutputStream(photoFile);
+                imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+                out.flush();
+                out.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             result.setImageBitmap(imageBitmap);
         }
     }
+
+
+    /*private File getFile(){
+        File folder = new File("sdcard/camera_app");
+        if(!folder.exists()){
+            folder.mkdir();
+        }
+        File imageFile = new File(folder,"cam_image.jpg");
+        return imageFile;
+    }*/
+
+    File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFIleName = "IMAGE"+timeStamp+"_";
+        File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        File image = File.createTempFile(imageFIleName,".jpg",storageDirectory);
+        mFileLocation = image.getAbsolutePath();
+
+        return image;
+    }
+
+    
 }
 
